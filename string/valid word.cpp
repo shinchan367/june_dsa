b046a537bd3adcdc12c84c27073e959241f505ae
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2025-07-15 12:49:43 +0530

    Create valid word.cpp

---
+++ string/valid word.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        unordered_set<char>s={'a','e','i','o','u','A','E','I','O','U'};
+        int n=word.size();
+        if(n<3) return false;
+        bool df=false,lf=false,uf=false,vf=false,cf=false;
+
+        for( auto c:word){
+            if(!isdigit(c) && !isalpha(c)) return false;
+            if(islower(c)) lf=true;
+            if(isupper(c)) uf=true;
+            if(isdigit(c))   df=true;
+            if(s.count(c)) vf=true;
+            else if(!s.count(c) && isalpha(c)) cf=true;
+        }
+        return ((df||uf||lf)&vf &cf);
+    }
+};
+
+
+
+class Solution:
+    def isValid(self, word: str) -> bool:
+        vowels = set('aeiouAEIOU')
+        n = len(word)
+        if n < 3:
+            return False
+
+        df = lf = uf = vf = cf = False
+
+        for c in word:
+            if not c.isdigit() and not c.isalpha():
+                return False
+            if c.islower():
+                lf = True
+            if c.isupper():
+                uf = True
+            if c.isdigit():
+                df = True
+            if c in vowels:
+                vf = True
+            elif c.isalpha():
+                cf = True
+
+        return (df or uf or lf) and vf and cf
