0dc29ae27fad116313ad245df4bd6cc0ff761d48
Sanam manojna <98442984+shinchan367@users.noreply.github.com>
2025-07-15 12:51:54 +0530

    Update valid word.cpp

--- string/valid word.cpp
+++ string/valid word.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     bool isValid(string word) {
+        if(word.length() < 3) {
+            return false;
+        }
+
+        bool hasVowel     = false;
+        bool hasConsonant = false;
+
+        for(char ch : word) {
+            if(isalpha(ch)) {
+                ch = tolower(ch);
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if(!isdigit(ch)) {
+                return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+};
+
+
+class Solution {
+public:
+    bool isValid(string word) {
         unordered_set<char>s={'a','e','i','o','u','A','E','I','O','U'};
         int n=word.size();
         if(n<3) return false;
